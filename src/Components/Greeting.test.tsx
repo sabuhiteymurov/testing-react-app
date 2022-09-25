c22ac5c26bebaa744bae749420fa9bc779f81793
Sabuhi Teymurov <teymurov.sabuhi@gmail.com>
2022-09-25 19:08:01 +0400

    Unit & Integration tests

--- src/Components/Greeting.test.tsx
+++ src/Components/Greeting.test.tsx
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Greeting from './Greeting';
 
 describe('Greeting component', () => {
-  test('renders Hello world as a text', () => {
+  test('renders "Hello world" as a text', () => {
     // The three 'A's Arrange, Act, Assert
     // Arrange
     render(<Greeting />);
@@ -12,5 +13,37 @@ describe('Greeting component', () => {
     // Assert
     const helloWorldElement = screen.getByText('Hello world', { exact: false });
     expect(helloWorldElement).toBeInTheDocument();
+  });
+
+  test('renders "good to see you" if the button was NOT clicked', () => {
+    render(<Greeting />);
+
+    const outputElement = screen.getByText('good to see you', { exact: false });
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test('renders "Changed" if the button was clicked', () => {
+    // Arrange
+    render(<Greeting />);
+
+    // Act
+    const buttonElement = screen.getByRole('button');
+    userEvent.click(buttonElement);
+
+    // Assert
+    const outputElement = screen.getByText('Changed!');
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test('does not render "good to see you" if the button was clicked', () => {
+    render(<Greeting />);
+
+    const buttonElement = screen.getByRole('button');
+    userEvent.click(buttonElement);
+
+    const outputElement = screen.queryByText('good to see you', {
+      exact: false,
+    });
+    expect(outputElement).toBeNull();
   });
 });
--- src/Components/Greeting.tsx
+++ src/Components/Greeting.tsx
@@ -1,8 +1,18 @@
+import { useState } from 'react';
+
 const Greeting = () => {
+  const [changedText, setChangedText] = useState(false);
+
+  const handleTextChange = () => {
+    setChangedText(true);
+  };
+
   return (
     <div>
       <h2>Hello world!</h2>
-      <p>It's good to see you!</p>
+      {!changedText && <p>It's good to see you!</p>}
+      {changedText && <p>Changed!</p>}
+      <button onClick={handleTextChange}>Change Text!</button>
     </div>
   );
 };
